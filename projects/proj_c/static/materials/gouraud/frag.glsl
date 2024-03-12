6d683fa8a5d5d916e44889a817c53def2f195699
Shrimp-Chip <esample21@gmail.com>
2024-03-12 00:09:53 -0400

    World position

--- projects/proj_c/static/materials/gouraud/frag.glsl
+++ projects/proj_c/static/materials/gouraud/frag.glsl
@@ -1,10 +1,11 @@
 precision mediump float;
 
-// Interpolated color from the vertex shader
+// varying variables -- passed from vertex shader
 varying vec4 v_color;
 varying vec2 v_uv;
 
+// Main fragment shader program
 void main() {
+    // Output the color interpolated from the vertices
     gl_FragColor = v_color;
-    // gl_FragColor = vec4(v_uv.x, v_uv.y, 0.0, 1.0) * v_color;
 }
--- projects/proj_c/static/materials/gouraud/vert.glsl
+++ projects/proj_c/static/materials/gouraud/vert.glsl
@@ -1,5 +1,3 @@
-// vert.glsl
-// Contains a basic vertex shader
 precision mediump float;
 
 struct Light {
@@ -9,7 +7,8 @@ struct Light {
     int lightType; // 0 for point light, 1 for directional light
 };
 
-struct LightBuffer {
+struct LightBuffer
+{
     Light lights[16];
     int numLights;
 };
@@ -24,7 +23,6 @@ uniform mat4 u_viewMatrix;
 uniform mat4 u_projectionMatrix;
 uniform mat4 u_modelMatrix;
 uniform float u_enable_lighting;
-
 uniform vec3 u_cameraPosition;
 uniform vec4 u_color;
 uniform float u_diffuse_influence;
@@ -33,14 +31,21 @@ uniform float u_frensel_influence;
 uniform vec4 u_frensel_color;
 uniform float u_frensel_border;
 uniform float u_show_grid;
-uniform LightBuffer u_lightBuffer;
-
+// constants
 const vec4 ambientLightColor = vec4(0.44, 0.45, 0.49, 1.0);
+const float cellShadingWeight = 0.4;
 const float gridSize = 0.5;
 const vec4 gridColor = vec4(0.6, 0.52, 0.85, 1.0);
 
+uniform LightBuffer u_lightBuffer;
+
+// varying variables to pass to fragment shader
 varying vec4 v_color;
 varying vec2 v_uv;
+
+float nStep(float x, float numSteps) {
+    return floor(x * numSteps) / float(numSteps);
+}
 
 vec4 lerp(vec4 a, vec4 b, float t) {
     return a * (1.0 - t) + b * t;
@@ -48,20 +53,19 @@ vec4 lerp(vec4 a, vec4 b, float t) {
 
 vec3 calculatePointLightDiffuse(Light light, vec4 position, vec4 normal) {
     vec3 lightDirection = normalize(light.position - position.xyz);
-    return lightDirection;
     float lightDistance = length(light.position - position.xyz);
     float attenuation = 1.0 / (1.0 + 0.1 * lightDistance + 0.01 * lightDistance * lightDistance);
     float diffuse = max(dot(normal.xyz, lightDirection), 0.0);
     return diffuse * attenuation * light.intensity * light.color;
 }
 
-vec3 calculatePointLightSpecular(Light light, vec4 pos, vec4 normal)
+vec3 calculatePointLightSpecular(Light light, vec4 v_position, vec4 normal)
 {
-    vec3 lightDirection = normalize(light.position - pos.xyz);
-    vec3 viewDirection = normalize(u_cameraPosition - pos.xyz);
+    vec3 lightDirection = normalize(light.position - v_position.xyz);
+    vec3 viewDirection = normalize(u_cameraPosition - v_position.xyz);
     vec3 reflectDirection = reflect(-lightDirection, normal.xyz);
     float specular = pow(max(dot(viewDirection, reflectDirection), 0.0), 32.0);
-    float lightDistance = length(light.position - pos.xyz);
+    float lightDistance = length(light.position - v_position.xyz);
     float attenuation = 1.0 / (1.0 + 0.1 * lightDistance + 0.01 * lightDistance * lightDistance);
     return specular * attenuation * light.intensity * light.color;
 }
@@ -83,82 +87,67 @@ vec3 calculateDirectionalLightSpecular(Light light, vec4 position, vec4 normal)
     return specular * light.intensity * light.color;
 }
 
-// main
 void main() {
     // Transform the vertex position into screen space
     mat4 mvp = u_projectionMatrix * u_viewMatrix * u_modelMatrix;
-    vec4 viewPos = mvp * a_position;
-    vec4 worldPos = u_modelMatrix * a_position;
+    vec4 pos = mvp * a_position;
+    vec4 worldPosition = u_modelMatrix * a_position;
+    vec4 normal = normalize(u_modelMatrix * a_normal);
 
     // check that the vertex is in front of the camera
-    if(viewPos.z < 0.0) {
+    if(pos.z < 0.0) {
         // if not, set the vertex position to the origin
-        viewPos = vec4(0.0, 0.0, 0.0, 0.0);
+        pos = vec4(0.0, 0.0, 0.0, 0.0);
     }
 
-    viewPos = viewPos / viewPos.w;
-    gl_Position = viewPos;
 
-    if(u_enable_lighting == 0.0) {
-        vec4 normalColor = vec4(a_normal.x, a_normal.y, a_normal.z, 1.0) * 0.5 + 0.5;
-        // mix the normal color with the object color
-        v_color = normalColor;
-        return;
-    }
+    pos = pos / pos.w;
+    gl_Position = pos;
 
-    vec4 normal = u_modelMatrix * a_normal;
 
-    // calculate the diffuse light
-    vec4 diffuseLight = vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 specularLight = vec4(0.0, 0.0, 0.0, 1.0);
-
-    for(int i = 0; i < 1; i++) {
-        if(i >= u_lightBuffer.numLights) {
-            break;
-        }
-
-        Light light = u_lightBuffer.lights[i];
-        if(light.lightType == 0) {
-            // point light
-            diffuseLight += vec4(calculatePointLightDiffuse(light, worldPos, normal), 1.0);
-            specularLight += vec4(calculatePointLightSpecular(light, worldPos, normal), 1.0);
-        } else {
-            diffuseLight += vec4(calculateDirectionalLightDiffuse(light, worldPos, normal), 1.0);
-            specularLight += vec4(calculateDirectionalLightSpecular(light, worldPos, normal), 1.0);
-        }
-    }
-
-    // calculate the frensel effect
-    vec4 viewDirection = normalize(vec4(u_cameraPosition, 1.0) - worldPos);
-    float frensel = 1.0 - dot(normal, viewDirection);
-    // make sure that the frensel effect is positive
-    // sqrt(pow)
-    frensel = sqrt(frensel * frensel);
-    frensel = pow(frensel, 1.0 / u_frensel_border);
-
-    vec4 frenselColor = u_frensel_color * frensel;
+    // Initial color setup
     vec4 color = u_color;
-    // calculate the final color
-    color = color * (ambientLightColor + diffuseLight * u_diffuse_influence);
-    color += frenselColor * u_frensel_influence;
-    color += specularLight * u_specular_influence;
 
+    if(u_enable_lighting != 0.0) {
+        vec4 ambientLight = ambientLightColor;
+        vec4 diffuseLight = vec4(0.0);
+        vec4 specularLight = vec4(0.0);
 
-    // this is for the grid on the platform
-    // i didn't feel like texturing the model
+        for (int i = 0; i < 8; i++) {
+            if (i >= u_lightBuffer.numLights) {
+                break;
+            }
+            Light light = u_lightBuffer.lights[i];
+            if (light.lightType == 0) { // Point light
+                diffuseLight += vec4(calculatePointLightDiffuse(light, worldPosition, normal), 1.0);
+                specularLight += vec4(calculatePointLightSpecular(light, worldPosition, normal), 1.0);
+            } else { // Directional light
+                diffuseLight += vec4(calculateDirectionalLightDiffuse(light, worldPosition, normal), 1.0);
+                specularLight += vec4(calculateDirectionalLightSpecular(light, worldPosition, normal), 1.0);
+            }
+        }
+
+        // Calculate the Fresnel effect
+        vec4 viewDirection = normalize(vec4(u_cameraPosition, 1.0) - worldPosition);
+        float frensel = pow(max(1.0 - dot(normal, viewDirection), 0.0), u_frensel_border);
+        vec4 frenselColor = u_frensel_color * frensel;
+
+        color = color * (ambientLight + diffuseLight * u_diffuse_influence + specularLight * u_specular_influence) + frenselColor * u_frensel_influence;
+    }
 
     // add a grid to the object, based on x and z coordinates, but only if the normal is pointing up
     float grid = 0.0;
-    if(mod(viewPos.x, gridSize * 10.0) < gridSize || mod(viewPos.z, gridSize * 10.0) < gridSize) {
+    if(mod(worldPosition.x, gridSize * 10.0) < gridSize || mod(worldPosition.z, gridSize * 10.0) < gridSize) {
         grid = 1.0;
     }
 
     // multiply it by the dot product of the normal and the up vector
+
     if(u_show_grid > 0.0) {
         grid *= max(dot(normal, vec4(0, 1, 0, 0)), 0.0);
 
         // multiply the grid by the distance from the origin
-        grid *= 1.0 - (length(viewPos) / 60.0);
+        grid *= 1.0 - (length(worldPosition) / 60.0);
 
         // clamp the grid value
         grid = clamp(grid, 0.0, 1.0);
@@ -167,6 +156,7 @@ void main() {
         color = lerp(color, gridColor, grid);
     }
 
-    v_uv = a_uv;
+    // Pass the computed color and texture coordinates to the fragment shader
     v_color = color;
+    v_uv = a_uv;
 }
